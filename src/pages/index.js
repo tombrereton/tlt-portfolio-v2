16411bf3dd374f04fddae9aa57b4cff6d5504959
tombrereton <tbrereton89@gmail.com>
2020-05-06 00:32:32 +0100

    font change

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,16 @@ const IndexPage = () => (
     <div style={sunStyles}>
       <Image />
     </div>
-    <p>Tom is a web developer and software architect based in London.</p>
+    <p><span style={{color: '#ffffff'}}>Tom</span> is a web developer and software architect based in <span style={{color: '#ffffff'}}>London</span>.</p>
     <p>My specialties range from building websites for restaurants to complex cloud solutions.</p>
-    <h1 style={{color:'#EB4C49'}}>PROJECTS</h1>
-    <h1 style={{color:'#E03D5B'}}>WORK</h1>
-    <h1 style={{color:'#C54F81'}}>CONTACT</h1>
+    <h1 style={{ color: '#EB4C49' }}>PROJECTS</h1>
+    <p>projects placeholder</p>
+    <h1 style={{ color: '#E03D5B' }}>WORK</h1>
+    <p>work placeholder</p>
+    <h1 style={{ color: '#C54F81' }}>CONTACT</h1>
+    <p>tom@tomlewis.tech</p>
+    <p><a href="https://www.linkedin.com/in/tbrereton/">LinkedIn</a></p>
+    <p><a href=" https://github.com/tombrereton">Github</a></p>
   </Layout>
 )
 
@@ -23,6 +28,8 @@ var sunStyles = {
   // display: 'flex',
   // justifyContent: 'center',
   width: `250px`,
+  margin: 'auto',
+  marginBottom: '2em'
 }
 
 
